d09fb1070f6f6583cc4929209e22f9cf098fc029
ali.sirin <ali.sirin@epias.com.tr>
2021-03-10 13:56:23 +0300

    feat: add cache evict service

--- src/main/java/com/example/caching/hazelcast/demo/controller/BookController.java
+++ src/main/java/com/example/caching/hazelcast/demo/controller/BookController.java
@@ -4,6 +4,7 @@ import com.example.caching.hazelcast.demo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class BookController {
     @GetMapping("/{isbn}")
     public String getBookNameByIsbn(@PathVariable("isbn") String isbn) {
         return bookService.getBookNameByIsbn(isbn);
+    }
+
+    @PostMapping(value = {"/evict-all-caches"})
+    public void evictAllCacheValues() {
+        bookService.evictAllCacheValues();
     }
 }
--- src/main/java/com/example/caching/hazelcast/demo/service/BookService.java
+++ src/main/java/com/example/caching/hazelcast/demo/service/BookService.java
@@ -1,5 +1,6 @@
 package com.example.caching.hazelcast.demo.service;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +15,14 @@ public class BookService {
     private String findBookInSlowSource(String isbn) {
         // some long processing
         try {
-            Thread.sleep(3000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "Sample Book Name";
+    }
+
+    @CacheEvict(value = "books", allEntries = true)
+    public void evictAllCacheValues() {
     }
 }
